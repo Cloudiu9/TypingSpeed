5cf4534293250fdf79dc1b125f3af69ffc3710d8
Cloudiu9 <claudiubordea5@gmail.com>
2026-07-06 16:21:30 +0300

    Finishing first HTML structure, removing overlay and starting timer functions.

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+// Hiding overlay and starting race
+const btnStart = document.querySelector<HTMLButtonElement>("#start");
+const timerEl = document.querySelector<HTMLSpanElement>("#timer");
+const overlayEl = document.querySelector<HTMLDivElement>("#overlay");
+
+function startRun() {
+  let SECONDS = 5;
+  // hide overlay
+  overlayEl?.classList.add("hidden");
+
+  // start clock
+  const timer = setInterval(() => {
+    SECONDS--;
+    if (timerEl && SECONDS >= 0) {
+      timerEl.textContent = "0:" + `${SECONDS}`.padStart(2, "0");
+    }
+
+    if (SECONDS < 0) {
+      clearInterval(timer);
+      alert("You type X WPM");
+      // TODO After confirming the alert/button that pops up, reset everything? ==> randomize text, add overlay back, reset timer
+    }
+  }, 1000);
+}
+
+btnStart?.addEventListener("click", startRun);
